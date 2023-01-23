404cb94b12f00c5bf51e53ef34e55c0c2a8ba135
NikhileshwarKore <nikhileshkore@gmail.com>
2023-01-23 19:32:50 +0530

    ..

--- force-app/main/default/lwc/list/list.js
+++ force-app/main/default/lwc/list/list.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
-
-export default class List extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import messagechannel from '@salesforce/messageChannel/SearchMessagingChannel__c';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+ 
+export default class list extends LightningElement {
+    publisherMessage = '';
+    subscription = null;
+ 
+    @wire(MessageContext)
+    messageContext;
+ 
+    connectedCallback() {
+        this.handleSubscribe();
+    }
+    handleSubscribe() {
+        if (this.subscription) {
+            return;
+        }
+        this.subscription = subscribe(this.messageContext, messagechannel, (message) => {
+            console.log(message.message);
+            this.publisherMessage = message.message;
+            this.ShowToast('Success', message.message, 'success', 'dismissable');
+        });
+    }
+    ShowToast(title, message, variant, mode){
+        const evt = new ShowToastEvent({
+            title: title,
+            message:message,
+            variant: variant,
+            mode: mode
+        });
+        this.dispatchEvent(evt);
+    }
+     
+}
--- force-app/main/default/lwc/search/search.js
+++ force-app/main/default/lwc/search/search.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
-
-export default class Search extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import messagechannel from '@salesforce/messageChannel/SearchMessagingChannel__c';
+import {publish, MessageContext} from 'lightning/messageService'
+ 
+export default class search extends LightningElement {
+    @wire(MessageContext)
+    messageContext;
+    message;
+ 
+    handleChange(event){
+        this.message = event.detail.value;
+    }
+ 
+    handleClick() {
+        let message = {message: this.message};
+        publish(this.messageContext, messagechannel, message);
+    }
+}

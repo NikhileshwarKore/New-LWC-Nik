dac641105c5ab90ad794014f0a040d2bb662385a
NikhileshwarKore <nikhileshkore@gmail.com>
2023-01-23 19:34:47 +0530

    m

--- force-app/main/default/aura/assignment/assignmentController.js
+++ force-app/main/default/aura/assignment/assignmentController.js
@@ -1,108 +1,4 @@
 ({  
-  openModel: function(component, event, helper) {
-      component.set("v.isModalOpen", true);
-    
-       var name1= component.find("inputname1").get("v.value");
-       var count1 = component.find("input1").get("v.value");
-       var name2 = component.find("inputname2").get("v.value");
-       var count2 = component.find("input2").get("v.value");
-       var name3= component.find("inputname3").get("v.value");
-       var count3 = component.find("input3").get("v.value");
-       var name4 = component.find("inputname4").get("v.value");
-       var count4 = component.find("input4").get("v.value"); 
-       var name5= component.find("inputname5").get("v.value");
-       var count5 = component.find("input5").get("v.value");
-       var name6 = component.find("inputname6").get("v.value");
-       var count6 = component.find("input6").get("v.value"); 
-       var name7= component.find("inputname7").get("v.value");
-       var count7 = component.find("input7").get("v.value");
-       var name8 = component.find("inputname8").get("v.value");
-       var count8 = component.find("input8").get("v.value"); 
-       var name9= component.find("inputname9").get("v.value");
-       var count9 = component.find("input9").get("v.value");
-       var name10 = component.find("inputname10").get("v.value");
-       var count10 = component.find("input10").get("v.value"); 
-       var name11= component.find("inputname11").get("v.value");
-       var count11 = component.find("input11").get("v.value");
-       var name12 = component.find("inputname12").get("v.value");
-       var count12 = component.find("input12").get("v.value"); 
-       var name13= component.find("inputname13").get("v.value");
-       var count13 = component.find("input13").get("v.value");
-       var name14 = component.find("inputname14").get("v.value");
-       var count14 = component.find("input14").get("v.value"); 
-       var name15= component.find("inputname15").get("v.value");
-       var count15 = component.find("input15").get("v.value");
-       var name16 = component.find("inputname16").get("v.value");
-       var count16 = component.find("input16").get("v.value"); 
-       var name17= component.find("inputname17").get("v.value");
-       var count17 = component.find("input17").get("v.value");
-       var name18 = component.find("inputname18").get("v.value");
-       var count18 = component.find("input18").get("v.value"); 
-       var name19= component.find("inputname19").get("v.value");
-       var count19 = component.find("input19").get("v.value");
-       var name20 = component.find("inputname20").get("v.value");
-       var count20 = component.find("input20").get("v.value"); 
-       var name21 = component.find("inputname21").get("v.value");
-       var count21 = component.find("input21").get("v.value");  
-        var name22 = component.find("inputname22").get("v.value");
-       var count22 = component.find("input22").get("v.value");  
- var custs = [];
- 
-       custs.push({value:count1, key:name1});
-       custs.push({value:count2, key:name2});
-        custs.push({value:count3, key:name3});
-      custs.push({value:count4, key:name4});
-      custs.push({value:count5, key:name5});
-      custs.push({value:count6, key:name6});
-      custs.push({value:count7, key:name7});
-      custs.push({value:count8, key:name8});
-      custs.push({value:count9, key:name9});
-      custs.push({value:count10, key:name10});
-      custs.push({value:count11, key:name11});
-      custs.push({value:count12, key:name12});
-      custs.push({value:count13, key:name13});
-      custs.push({value:count14, key:name14});
-      custs.push({value:count15, key:name15});
-      custs.push({value:count16, key:name16});
-      custs.push({value:count17, key:name17});
-      custs.push({value:count18, key:name18});
-      custs.push({value:count19, key:name19});
-      custs.push({value:count20, key:name20});
-      custs.push({value:count21, key:name21});
-      custs.push({value:count22, key:name22});
-       
-       
-       component.set("v.totalCount",custs);
-       console.log("v.totalCount-------"+JSON.stringify(custs));
-   },
-     doInit : function(component) {
-        var action  = component.get('c.getRec');
-        action.setCallback(this,function(response){
-            var state = response.getState();
-            if(state == 'SUCCESS'){
-                var returnData = response.getReturnValue();
-                component.set("v.getResources",returnData);
-            }
-        });
-        $A.enqueueAction(action);
-        
-    } ,
-  
-   closeModel: function(component, event, helper) {  
-          component.set("v.isModalOpen", false);
-   },
-  
-   submitDetails: function(component, event, helper) {
-      component.set("v.isModalOpen", false);
-      },    
-   increment : function(component, event, helper) {
-       var count = parseInt (component.get("v.count")+1);
-          component.set("v.count",count);
-    },
-    decrement : function(component, event, helper) {
-        var count=parseInt (component.get("v.count")-1);
-        component.set("v.count",count);
-    },
      increment1 : function(component, event, helper) {
        var count = parseInt (component.get("v.count1")+1);
           component.set("v.count1",count);
@@ -329,4 +225,132 @@ var count = parseInt(component.get("v.count22")-1);
 component.set("v.count22", count);
          },   
   
-});
+  openModel: function(component, event, helper) {
+      component.set("v.isModalOpen", true);
+    
+       var name1= component.find("inputname1").get("v.value");
+       var count1 = component.find("input1").get("v.value");
+       var name2 = component.find("inputname2").get("v.value");
+       var count2 = component.find("input2").get("v.value");
+       var name3= component.find("inputname3").get("v.value");
+       var count3 = component.find("input3").get("v.value");
+       var name4 = component.find("inputname4").get("v.value");
+       var count4 = component.find("input4").get("v.value"); 
+       var name5= component.find("inputname5").get("v.value");
+       var count5 = component.find("input5").get("v.value");
+       var name6 = component.find("inputname6").get("v.value");
+       var count6 = component.find("input6").get("v.value"); 
+       var name7= component.find("inputname7").get("v.value");
+       var count7 = component.find("input7").get("v.value");
+       var name8 = component.find("inputname8").get("v.value");
+       var count8 = component.find("input8").get("v.value"); 
+       var name9= component.find("inputname9").get("v.value");
+       var count9 = component.find("input9").get("v.value");
+       var name10 = component.find("inputname10").get("v.value");
+       var count10 = component.find("input10").get("v.value"); 
+       var name11= component.find("inputname11").get("v.value");
+       var count11 = component.find("input11").get("v.value");
+       var name12 = component.find("inputname12").get("v.value");
+       var count12 = component.find("input12").get("v.value"); 
+       var name13= component.find("inputname13").get("v.value");
+       var count13 = component.find("input13").get("v.value");
+       var name14 = component.find("inputname14").get("v.value");
+       var count14 = component.find("input14").get("v.value"); 
+       var name15= component.find("inputname15").get("v.value");
+       var count15 = component.find("input15").get("v.value");
+       var name16 = component.find("inputname16").get("v.value");
+       var count16 = component.find("input16").get("v.value"); 
+       var name17= component.find("inputname17").get("v.value");
+       var count17 = component.find("input17").get("v.value");
+       var name18 = component.find("inputname18").get("v.value");
+       var count18 = component.find("input18").get("v.value"); 
+       var name19= component.find("inputname19").get("v.value");
+       var count19 = component.find("input19").get("v.value");
+       var name20 = component.find("inputname20").get("v.value");
+       var count20 = component.find("input20").get("v.value"); 
+       var name21 = component.find("inputname21").get("v.value");
+       var count21 = component.find("input21").get("v.value");  
+        var name22 = component.find("inputname22").get("v.value");
+       var count22 = component.find("input22").get("v.value");  
+ var custs = [];
+ 
+       custs.push({value:count1, key:name1});
+       custs.push({value:count2, key:name2});
+        custs.push({value:count3, key:name3});
+      custs.push({value:count4, key:name4});
+      custs.push({value:count5, key:name5});
+      custs.push({value:count6, key:name6});
+      custs.push({value:count7, key:name7});
+      custs.push({value:count8, key:name8});
+      custs.push({value:count9, key:name9});
+      custs.push({value:count10, key:name10});
+      custs.push({value:count11, key:name11});
+      custs.push({value:count12, key:name12});
+      custs.push({value:count13, key:name13});
+      custs.push({value:count14, key:name14});
+      custs.push({value:count15, key:name15});
+      custs.push({value:count16, key:name16});
+      custs.push({value:count17, key:name17});
+      custs.push({value:count18, key:name18});
+      custs.push({value:count19, key:name19});
+      custs.push({value:count20, key:name20});
+      custs.push({value:count21, key:name21});
+      custs.push({value:count22, key:name22});
+       
+       
+       component.set("v.totalCount",custs);
+       console.log("v.totalCount-------"+JSON.stringify(custs));
+   },
+       closeModel: function(component, event, helper) {
+      // Set isModalOpen attribute to false  
+      component.set("v.isModalOpen", false);
+   },
+  
+   submitDetails: function(component, event, helper) {
+      // Set isModalOpen attribute to false
+      //Add your code to call apex method or do some processing
+      component.set("v.isModalOpen", false);
+   },
+ 
+    saveRecord : function(component, event, helper) {
+        var action = component.get("c.saveDataToServer");
+        action.setParams({ P1: component.get("v.count1"),
+   P2: component.get("v.count2"),
+   P3: component.get("v.count3"),
+     P4: component.get("v.count4"),
+  P5: component.get("v.count5"),
+ P6: component.get("v.count7"),
+ P7: component.get("v.count7"),
+ P8: component.get("v.count8"),
+ P9: component.get("v.count9"),
+ P10: component.get("v.num10"),
+ P11: component.get("v.count11"),
+ P12: component.get("v.count12"),
+ P13: component.get("v.count13"),
+ P14: component.get("v.count14"),
+ P15: component.get("v.count15"),
+ P16: component.get("v.count16"),
+ P17: component.get("v.count17"),
+ P18: component.get("v.count18"),
+ P19: component.get("v.count19"),
+ P20: component.get("v.count20"),
+ P21: component.get("v.count21"),
+ P22: component.get("v.count22"),                          
+                         });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+             alert('Record Created Successfully!!');   // handle the success case
+            } else {
+               var errors = action.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        alert(errors[0].message); // handle the error case
+            }
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    }
+   
+})

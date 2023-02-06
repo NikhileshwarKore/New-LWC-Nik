4a0f61daa9c81ea4de7449dd635920e56920e561
NikhileshwarKore <nikhileshkore@gmail.com>
2023-02-06 18:18:29 +0530

    a

---
+++ force-app/main/default/lwc/lds/lds.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import bulmacss from '@salesforce/resourceUrl/bulmacss';
+export default class Bulma extends LightningElement {
+    connectedCallback() {
+        loadStyle(this, bulmacss);
+    }
+}
